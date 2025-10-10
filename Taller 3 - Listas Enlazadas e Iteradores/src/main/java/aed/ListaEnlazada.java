69b01a6e4cf1cd24a5f190a605ed93550afdd0ed
Alejandro Leupold de Souza <aleupold@dappsfactory.io>
2025-10-10 12:27:11 -0300

    taller3 a casa

--- Taller 3 - Listas Enlazadas e Iteradores/src/main/java/aed/ListaEnlazada.java
+++ Taller 3 - Listas Enlazadas e Iteradores/src/main/java/aed/ListaEnlazada.java
@@ -1,23 +1,26 @@
 package aed;
 
-public class ListaEnlazada<T> {
-    private Nodo nodoActual;
+public class ListaEnlazada<T>  {
+    private Nodo primero;
+    private Nodo ultimo;
+
     private int longitudLista;
 
     private class Nodo {
-       public T anterior;
-       public T valor;
-       public T siguiente;
+        public T valor;
+        public Nodo siguiente;
+        public Nodo anterior;
+        public Nodo(T v) { 
+            valor = v; 
+        }
 
-       public Nodo(T v) { valor = v; }
-
-       public void asignarPunteros(T an, T sig){ anterior = an;  siguiente = sig;}
+        public void asignarPunteros(Nodo an, Nodo sig){ anterior = an;  siguiente = sig;}
 
     }
 
     public ListaEnlazada() {
-        nodoActual = new Nodo(null);
-        nodoActual.asignarPunteros(null, null);
+        primero = null;
+        ultimo = null;
         longitudLista = 0;
     }
 
@@ -25,62 +28,130 @@ public class ListaEnlazada<T> {
         return longitudLista;
     }
 
+
     public void agregarAdelante(T elem) {
-        Nodo nuevoNodo = new Nodo(elem);
-        nuevoNodo.asignarPunteros(null, nodoActual.valor);
+        Nodo nodoNuevo = new Nodo(elem); 
+        if (primero == null && ultimo == null){
+            nodoNuevo.asignarPunteros(null, null);
+            primero = nodoNuevo;
+            ultimo = nodoNuevo;
+        }
+        else{
+            Nodo versionAnteriorAPrimero = primero;
+            primero = nodoNuevo;
+            primero.asignarPunteros(null, versionAnteriorAPrimero);
+            versionAnteriorAPrimero.anterior = primero;
+        };
         longitudLista++;
     }
 
     public void agregarAtras(T elem) {
-        Nodo nuevoNodo = new Nodo(elem);
-        nuevoNodo.asignarPunteros( nodoActual.valor, null );
-        longitudLista++;    
+        Nodo nodoNuevo = new Nodo(elem); 
+        if (primero == null && ultimo == null){
+            nodoNuevo.asignarPunteros(null, null);
+            primero = nodoNuevo;
+            ultimo = nodoNuevo;
+        }
+        else{
+            // Nodo antePenultimoNodo = ultimo.anterior;
+            Nodo versionAnteriorUltimo = ultimo;
+            ultimo = nodoNuevo;
+            ultimo.asignarPunteros(versionAnteriorUltimo, null);
+            versionAnteriorUltimo.siguiente = ultimo;
+        };
+        longitudLista++;
+    }
+
+    private Nodo obtenerNodo(int i){
+         Nodo nodoAObtener = primero;
+        int j = 0;
+        while (j < i){
+            nodoAObtener = nodoAObtener.siguiente;
+            j++;
+        };
+        return nodoAObtener;
     }
 
     public T obtener(int i) {
-        throw new UnsupportedOperationException("No implementada aun");
+        return obtenerNodo(i).valor;
+    }
+
+    private void desconectarNodo (Nodo anteriorAEliminar, Nodo siguienteAEliminar){
+         if (anteriorAEliminar != null){
+            anteriorAEliminar.siguiente = siguienteAEliminar;
+        }else{
+            primero = siguienteAEliminar;
+        }
+
+        if (siguienteAEliminar != null){
+            siguienteAEliminar.anterior = anteriorAEliminar;
+        }else{
+            ultimo = anteriorAEliminar;
+        }
     }
 
     public void eliminar(int i) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodoAEliminar = obtenerNodo(i);
+        System.out.println(nodoAEliminar.valor);
+
+        Nodo anteriorAEliminar = nodoAEliminar.anterior;
+        Nodo siguienteAEliminar = nodoAEliminar.siguiente;
+        desconectarNodo(anteriorAEliminar, siguienteAEliminar);
+        longitudLista--;
+        
     }
 
     public void modificarPosicion(int indice, T elem) {
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodoAModificar = obtenerNodo(indice);
+        nodoAModificar.valor = elem;
     }
 
     public ListaEnlazada(ListaEnlazada<T> lista) {
-        throw new UnsupportedOperationException("No implementada aun");
+        int i = 0;
+        while (i < lista.longitud()){
+            agregarAtras(lista.obtener(i));
+            i++;
+        }
     }
     
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("No implementada aun");
+        String listaAImprimir = new String("[");
+        int i = 0;
+        while(i < longitudLista - 1){
+            listaAImprimir += obtener(i) + ", ";
+            i++;
+        }
+
+        listaAImprimir += ultimo.valor + "]";
+        return listaAImprimir;
     }
 
-    public class ListaIterador{
+    public class ListaIterador implements Iterador<T>{
     	// Completar atributos privados
-
+        int posc = 0;
         public boolean haySiguiente() {
-	        throw new UnsupportedOperationException("No implementada aun");
+            return posc >=0 && posc < longitudLista;
         }
         
         public boolean hayAnterior() {
-	        throw new UnsupportedOperationException("No implementada aun");
+            return posc > 0 && posc <= longitudLista;
         }
 
         public T siguiente() {
-	        throw new UnsupportedOperationException("No implementada aun");
+            T valorADevolver = obtener(posc);
+            posc++;
+            return valorADevolver;
         }
         
 
         public T anterior() {
-	        throw new UnsupportedOperationException("No implementada aun");
-        }
+            posc--;
+            T valorADevolver = obtener(posc);
+            return valorADevolver;        }
     }
-
     public ListaIterador iterador() {
-	    throw new UnsupportedOperationException("No implementada aun");
+        return new ListaIterador();
+    
     }
-
 }
